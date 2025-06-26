cc55e754b12262b2b376fbfb40d741f074e76ff0
Esanki Thambawita <esankilakvindee2000@gmail.com>
2025-06-26 20:50:21 +0530

    Added two users

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser"; //take in incoming post request bodies
 
+import usersRoutes from "./routes/users.js";
+
 //Initialize the express app
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json()); //Gonna use json data in the entire application
+
+app.use("/users", usersRoutes);
 
 //Listen to incoming requests
 app.listen(PORT, () =>
@@ -13,7 +17,4 @@ app.listen(PORT, () =>
 ); //callback function which is going to be executed once we run the server
 
 //create a route
-app.get("/", (req, res) => {
-  console.log("Test!");
-  res.send("Hello");
-});
+app.get("/", (req, res) => res.send("Hello from homepage"));
---
+++ routes/users.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const router = express.Router();
+
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 24,
+  },
+];
+
+//All routes here are starting with /users
+router.get("/", (req, res) => {
+  console.log(users);
+  res.send(users);
+});
+
+export default router;
