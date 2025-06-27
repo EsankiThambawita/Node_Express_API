d379dd3bdba3bbe9d311099783d0eb2f636dba32
Esanki Thambawita <esankilakvindee2000@gmail.com>
2025-06-27 17:58:00 +0530

    Update users.js
    Implemented delete, patch methods

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 const router = express.Router();
 
 //mock database
-const users = [];
+let users = [];
 
 //All routes here are starting with /users
 router.get("/", (req, res) => {
@@ -26,6 +26,28 @@ router.get("/:id", (req, res) => {
   const foundUser = users.find((user) => user.id == id);
 
   res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id !== id);
+
+  res.send(`User with the id "${id}" deleted from the database.`);
+});
+
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const { firstName, lastName, age } = req.body;
+
+  const user = users.find((user) => user.id == id);
+
+  if (firstName) user.firstName = firstName;
+  if (lastName) user.lastName = lastName;
+  if (age) user.age = age;
+
+  res.send(`User with the id "${id}" updated.`);
 });
 
 export default router;
